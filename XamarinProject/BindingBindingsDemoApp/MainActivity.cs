f00728624273a63b15a0ab944e111c2ae4129df6
Leo Langenhoven <leo.langenhoven@standardbank.co.za>
2018-04-04 17:30:54 +0200

    Create Xamarin android project that attempts to bind to example native module
    Created solution with 5 projects:
    - 3 bindings projects to include android databinding libraries
    - 1 bindings project that includes 'librarywithbinding-debug.aar' This native android library
    was created from the example native android project in this repo
    - 1 Xamarin android application that references the binding projects
    
    The xamarin android application contains one Activity with a button that navigates
    to the native android .aar module's Activity (which uses android-databinding)

---
+++ XamarinProject/BindingBindingsDemoApp/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Widget;
+
+namespace BindingBindingsDemoApp
+{
+    [Activity(Label = "BindingBindingsDemoApp", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+
+            button.Click += delegate {
+                var intent = new Intent(this, typeof(Databinding.Android.Librarywithbinding.BindingSampleActivity));
+                StartActivity(intent);
+            };
+        }
+    }
+}
+
